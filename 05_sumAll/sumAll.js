772b951f7c4050f071d076eb51e7a47e905610f5
jeff-dillon <dillonjg@gmail.com>
2022-10-17 17:52:08 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstValue, secondValue) {
+    if(typeof(firstValue) != "number" || typeof(secondValue) != "number") {
+        return 'ERROR';
+    } else if(firstValue < 0 || secondValue < 0) {
+        return 'ERROR';
+    } else {
+        if(firstValue < secondValue) {
+            start = firstValue;
+            end = secondValue;
+        } else {
+            start = secondValue;
+            end = firstValue;
+        }
+        let result = 0;
+        for(i = start; i <= end; i++) {
+            result += i;
+        }
+        return result;
 
+    }
 };
 
 // Do not edit below this line
