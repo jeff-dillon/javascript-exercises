63abd70d8f6db68c1966cfa7f3263d0435108051
jeff-dillon <dillonjg@gmail.com>
2022-10-17 18:00:40 -0400

    complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    const isDivisibleByFour = year % 4 == 0;
+    const isDivisibleByOneHundred = year % 100 == 0;
+    const isDivisibleByFourHundred = year % 400 == 0;
+
+    return (isDivisibleByFour && !isDivisibleByOneHundred) || isDivisibleByFourHundred;
 };
 
 // Do not edit below this line
