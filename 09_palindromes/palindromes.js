74241f0e1e69678210cf5ab7db3f785809b1f0dc
jeff-dillon <dillonjg@gmail.com>
2022-10-18 09:22:35 -0400

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const reverseString = function(myString) {
+    const myArray = myString.split("");
+    myArray.reverse();
+    const result = myArray.join("");
+    return result;
 };
+
+const palindromes = function (inputStr) {
+    const noPunctuationStr = inputStr.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    const noSpaceStr = noPunctuationStr.replace(/[\s]/g,"");
+    const lowerStr = noSpaceStr.toLowerCase();
+    const revStr = reverseString(lowerStr);
+    return lowerStr === revStr;
+};
+
 
 // Do not edit below this line
 module.exports = palindromes;
