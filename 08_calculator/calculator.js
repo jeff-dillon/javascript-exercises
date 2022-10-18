0b534291cc65db06a916a65cf894656731d08e77
jeff-dillon <dillonjg@gmail.com>
2022-10-17 20:28:41 -0400

    complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	let result = 0;
+  for(const num of numArray) {
+    result += num;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  let result = 1;
+  for(const num of numArray) {
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(firstNum, secondNum) {
+	return Math.pow(firstNum, secondNum);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num < 0) {
+    return -1;
+  } else if(num == 0) {
+    return 1;
+  } else {
+    return(num * factorial(num -1));
+  }
 };
 
 // Do not edit below this line
