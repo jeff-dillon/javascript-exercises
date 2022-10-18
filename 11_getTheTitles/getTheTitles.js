c61f152feaf34ec462d5de117ba5d220ba224fa0
jeff-dillon <dillonjg@gmail.com>
2022-10-18 09:47:07 -0400

    commplete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayOfMaps) {
+    const titleArray = [];
+    const numberOfMaps = arrayOfMaps.length;
+    for(let i = 0; i < numberOfMaps; i++) {
+        const currentMap = arrayOfMaps[i];
+        titleArray.push(currentMap["title"]);
+    }
+    return titleArray;
 };
 
 // Do not edit below this line
