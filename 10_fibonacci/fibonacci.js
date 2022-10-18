5a0f15392d6f0624ee6e41c1e0e9e62abf287f13
jeff-dillon <dillonjg@gmail.com>
2022-10-18 09:39:05 -0400

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(requestedIndex) {
+    const fibArray = [];
+    for(let i = 0; i <= 100; i++) {
+        if(fibArray.length == 0 || fibArray.length == 1) {
+            fibArray.push(1);
+        } else {
+            fibArray.push(fibArray[i-1] + fibArray[i-2]);
+        }
+    }
+    if(requestedIndex < 0) {
+        return "OOPS";
+    }
+    return fibArray[requestedIndex-1];
 };
 
 // Do not edit below this line
