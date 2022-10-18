d58d07a3ebad2ebd97a32bff99e8b6a0cd526ab2
jeff-dillon <dillonjg@gmail.com>
2022-10-18 14:46:59 -0400

    complete exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,39 @@
-const caesar = function() {
+const caesar = function(inputStr, shift) {
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let resultStr = "";
+    
+    for(let i = 0; i < inputStr.length; i++) {
+        
+        let character = inputStr.charAt(i);
+        
+        if(lower.includes(character)) {
 
+            let index = lower.indexOf(character);
+            let offset = index + shift;
+            if((offset + 1) > lower.length) {
+                offset = offset % lower.length;
+            } else if(offset < 0) {
+                offset = (Math.abs(offset) % lower.length) * -1;
+            }
+            resultStr += lower.slice(offset, offset + 1);
+
+        } else if (upper.includes(character)) {
+
+            let index = upper.indexOf(character)
+            let offset = index + shift;
+            if((offset +1) > upper.length) {
+                offset = offset % upper.length;
+            } else if(offset < 0) {
+                offset = (Math.abs(offset) % upper.length) * -1;
+            }
+            resultStr += upper.slice(offset, offset + 1);
+
+        } else {
+            resultStr += character;
+        }
+    }
+    return resultStr;
 };
 
 // Do not edit below this line
